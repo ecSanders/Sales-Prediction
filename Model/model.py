750c64a77aa4c0ccd3154731921b9a97c6db4f27
ecSanders <ecsander0@gmail.com>
2021-12-13 23:14:54 -0700

    Added annotations

--- Model/model.py
+++ Model/model.py
@@ -1,6 +1,5 @@
 #%% Import libraries
 import os
-import xgboost as xgb
 from xgboost import XGBRegressor
 import numpy as np
 import pandas as pd
@@ -9,20 +8,10 @@ from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import MinMaxScaler
 
-import tensorflow as tf
-import matplotlib.pyplot as plt
-
-from tensorflow import keras
-from tensorflow.keras import layers
-
-from sklearn.preprocessing import MinMaxScaler
-
 PATH = os.getcwd()[:-5]
 DATA_PATH = PATH + "Data Analysis\sales EDA.csv"
 df = pd.read_csv(DATA_PATH)
 df.drop(columns=['Unnamed: 0'], inplace=True)
-
-
 
 # %% One Hot Encode
 df_cat = df.select_dtypes(include='object')
@@ -35,8 +24,8 @@ df_cat = pd.DataFrame(results, columns=ohe.get_feature_names())
 df = pd.concat([df,df_cat],axis=1)
 df.dropna(inplace=True)
 
-# %%
-train_dataset = df.sample(frac=0.90, random_state=1)
+# %% Split and scale
+train_dataset = df.sample(frac=0.80, random_state=1)
 train_dataset = (train_dataset[(train_dataset.Order_Year == 2017) & 
         (train_dataset.x9_December == 1)])
 
@@ -53,123 +42,12 @@ mms = MinMaxScaler().fit(X_train)
 X_train = mms.transform(X_train)
 X_test = mms.transform(X_test)
 
-#%%
+#%% Fit, predict, evaluate
 xmod = XGBRegressor()
 xmod = (xmod.fit(X_train,y_train))
-y_hat = xmod.predict(X_test)
 y_hat = xmod.predict(X_test)
 r2_score(y_test, y_hat)
 mean_squared_error(y_test, y_hat, squared=False)
 
-
 #%%
-
-xmod.save_model('XGBmodel03.h5')
-# 0.6549956655283837
-#%%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#%%
-def plot_loss(history):
-  plt.plot(history.history['loss'], label='loss')
-  plt.plot(history.history['val_loss'], label='val_loss')
-
-  # Examine more closely
-  
-
-  plt.xlabel('Epoch')
-  plt.ylabel('Error')
-  plt.legend()
-  plt.grid(True)
-
-# Learnrate schedule
-def sketch(epoch, lr):
-  if epoch >= 500:
-    return 0.001
-  if epoch >= 440:
-    return 0.002
-  if epoch >= 100:
-    return 0.003
-  else:
-    return lr
-
-# Build model
-def build_model(norm):
-    model = keras.Sequential([
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-        layers.Dense(200, activation=tf.nn.leaky_relu),
-
-
-        #layers.Dense(100, activation=tf.nn.leaky_relu),
-        layers.Dense(1)
-    ])    
-        
-    (model.compile(optimizer=tf.optimizers.Nadam(0.001),
-                  loss='mean_squared_error',
-                  metrics=[tf.keras.metrics.MeanSquaredError()]))
-    return model
-
-# Sub b/c I'm lazy
-normalizer = 0
-model = build_model(normalizer)
-
-# Fit and stuff
-callback = keras.callbacks.LearningRateScheduler(schedule=sketch)
-history = model.fit(
-    X_train,
-    y_train,
-    batch_size=50,
-    validation_split=0.2,
-    epochs = 100,
-    # callbacks=[callback],
-    verbose=2
-)
-
-plot_loss(history)
-
-#%%
-# Learning rate #
-# 0.01 goodish
-# 0.03 XXX
-# 0.003 
-# %%
-y_hat = model.predict(X_test)
-r2_score(y_test,y_hat)
-# %%
-model.save('03NN.h5')
-# Best: 0.6318819258327169
-# %%
+xmod.save_model('XGBmodel04.h5')
